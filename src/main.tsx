90223075ae860a86efacaa67ded3940ea682643f
Young Jin <likeRudin@gmail.com>
2023-10-03 12:48:30 +0900

    INJECTION DEPENDENDCIES: themeprovider, recoilRote, globalstyle

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router.tsx";
+import { RecoilRoot } from "recoil";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import reset from "styled-reset";
+import { theme } from "./theme.ts";
+
+const GlobalStyle = createGlobalStyle`
+${reset}`;
 
 const rootElement = document.getElementById("root")!;
 const root = ReactDOM.createRoot(rootElement);
@@ -11,8 +18,13 @@ const client = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={client}>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
