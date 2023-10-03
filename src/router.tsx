548b18b736187359d03471625180d0b494e84ead
Young Jin <likeRudin@gmail.com>
2023-10-03 12:49:00 +0900

    ROUTERS: make backbone for project

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,15 @@
-import App from "./App";
 import { createBrowserRouter } from "react-router-dom";
-
+import { Popular, ComingSoon, NowPlaying } from "./routes";
+import App from "./App";
+import { PATH } from "./constants/constants.ts";
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      { path: `${PATH.POPULAR}`, element: <Popular /> },
+      { path: `${PATH.COMINGSOON}`, element: <ComingSoon /> },
+      { path: `${PATH.NOWPLAYING}`, element: <NowPlaying /> },
+    ],
   },
 ]);
