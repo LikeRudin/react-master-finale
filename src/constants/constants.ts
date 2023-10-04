62dde5e303549cfd1e3abe5b4319ab2ad7be5524
Young Jin <likeRudin@gmail.com>
2023-10-04 20:32:32 +0900

    CONSTANTS: make a page constant to prevent mistake

---
+++ src/constants/constants.ts
@@ -0,0 +1,11 @@
+export const enum PATH {
+  POPULAR = "/",
+  NOWPLAYING = "/now-playing",
+  COMINGSOON = "/coming-soon",
+  DISNEY = "/disney",
+  MARVEL = "/marvel",
+}
+
+export const enum LAYOUT_ID {
+  CIRCLE = "circle",
+}
