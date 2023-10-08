f0d719740846e9ca15c302bf6540c1f035fec82c
Young Jin <likeRudin@gmail.com>
2023-10-09 01:20:22 +0900

    FUNCTIONALITY: save Memory

---
+++ src/components/archive/saved-mordal.tsx
@@ -0,0 +1,113 @@
+import { ExtendedIContent, TextAreaSelector } from "../../atoms";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import React from "react";
+import { useRecoilState } from "recoil";
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: 3;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const ModalWrapper = styled(motion.div)`
+  margin-top: 300px;
+  width: 900px;
+  height: 700px;
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 30px;
+  display: flex;
+  padding: 20px;
+  position: absolute;
+  bottom: 10%;
+`;
+const InfoWrapper = styled.div`
+  height: 90%;
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`;
+const ControlWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 90%;
+  width: 45%;
+`;
+const Image = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`;
+const Name = styled.span`
+  font-size: xxx-large;
+  color: white;
+`;
+const SavedDate = styled.span`
+  font-size: x-large;
+  color: azure;
+`;
+const TextArea = styled.textarea`
+  font-size: xx-large;
+  background-color: rgba(255, 255, 255, 0.3);
+  color: white;
+  width: 100%;
+  height: 80%;
+`;
+const CloseButton = styled.button`
+  width: 100px;
+  height: 100px;
+`;
+
+interface SavedModalProps {
+  modalData: ExtendedIContent;
+  index: number;
+  handleClose: () => void;
+}
+export const SavedModal = ({
+  modalData,
+  index,
+  handleClose,
+}: SavedModalProps) => {
+  const [text, setText] = useRecoilState(TextAreaSelector({ index }));
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setText(event.currentTarget.value);
+  };
+  return (
+    <Overlay>
+      <ModalWrapper layoutId={`saved${index}`}>
+        <InfoWrapper>
+          <Image src={modalData.ImagePath} />
+          <Name>{modalData.name}</Name>
+          <SavedDate>saved at: {modalData.savedDate}</SavedDate>
+        </InfoWrapper>
+        <ControlWrapper>
+          <CloseButton onClick={handleClose}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 15l-6 6m0 0l-6-6m6 6V9a6 6 0 0112 0v3"
+              />
+            </svg>
+          </CloseButton>
+          <TextArea value={text} onChange={onChange}></TextArea>
+        </ControlWrapper>
+      </ModalWrapper>
+    </Overlay>
+  );
+};
--- src/components/archive/saved.tsx
+++ src/components/archive/saved.tsx
@@ -1,36 +1,56 @@
 import styled from "styled-components";
 import { Draggable } from "react-beautiful-dnd";
-
+import { motion } from "framer-motion";
 interface SavedProps {
   imagePath: string;
   name: string;
-  comment: string;
+  handleClick: () => void;
   id: number;
+  index: number;
 }
 
-const Wrapper = styled.div`
-  width: 200px;
-  height: 200px;
+const Wrapper = styled.div<{ isDragging: boolean }>`
+  width: 400px;
+  height: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) =>
+    props.isDragging ? "rgba(255, 255, 255, 0.5)" : "rgba(255, 255, 255, 0.2)"};
+  border-radius: 10px;
+  margin-right: 20px;
 `;
-const Name = styled.span``;
+const Name = styled.span`
+  font-size: xx-large;
+  color: white;
+`;
 const Image = styled.img`
-  width: 200px;
-  height: 200px;
+  max-width: 90%;
+  width: auto;
+  height: 300px;
+  object-fit: contain;
+  border: 1px solid black;
 `;
-const Comment = styled.textarea``;
 
-export const Saved = ({ imagePath, name, comment, id }: SavedProps) => {
+export const Saved = ({
+  imagePath,
+  name,
+  id,
+  index,
+  handleClick,
+}: SavedProps) => {
   return (
-    <Draggable draggableId={`${name}-${id}`}>
-      {(provided, snpashot) => (
+    <Draggable draggableId={`${name}-${id}`} index={index}>
+      {(provided, snapshot) => (
         <Wrapper
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
         >
-          <Image src={imagePath} />
+          <Image src={imagePath} onClick={handleClick} />
           <Name>{name}</Name>
-          <Comment>{comment}</Comment>
         </Wrapper>
       )}
     </Draggable>
